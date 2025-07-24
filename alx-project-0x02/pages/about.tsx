98e0692c7445baa1e97a5e921fd3e8671f2553b2
Mugishaa77 <Sallywanga2016@gmail.com>
2025-07-24 13:41:43 +0300

    Create and Use a Button Component

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,4 +1,5 @@
-import Header from "../components/layout/Header";
+import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 const About: React.FC = () => {
   return (
@@ -8,8 +9,14 @@ const About: React.FC = () => {
         <h1 className="text-4xl font-bold">About Us</h1>
         <p className="mt-4 text-gray-600">Learn more about our project and goals here.</p>
       </main>
+       <div className="p-6 space-x-4">
+      <Button title="Small Button" size="small" shape="rounded-sm" />
+      <Button title="Medium Button" size="medium" shape="rounded-md" />
+      <Button title="Large Button" size="large" shape="rounded-full" />
+    </div>
     </div>
   );
 };
 
 export default About;
+
